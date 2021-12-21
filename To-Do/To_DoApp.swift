03ebc02e90b90ad2e0006c2cce59792cd11fe2d6
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2021-12-21 15:38:17 +0200

    Initial Commit

---
+++ To-Do/To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoApp.swift
+//  To-Do
+//
+//  Created by Syber Expertise on 21/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
